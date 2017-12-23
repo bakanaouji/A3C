f3ed180eff9a24abb173c3180f86dddce3385c86
bakanaouji <abeken1224@yahoo.co.jp>
2017-12-23 23:45:29 +0900

    parser編集

--- src/main.py
+++ src/main.py
@@ -3,3 +3,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="A3C")
+    # 環境側のパラメータ
+    parser.add_argument('--env_name', default='PongNoFrameskip-v4', help='Environment name')
+    parser.add_argument('--width', type=int, default=84, help='Width of resized frame')
+    parser.add_argument('--height', type=int, default=84, help='Height of resized frame')
