c0aeb98f5f00dfd78cfff4122ddd55167b2810f9
bakanaouji <abeken1224@yahoo.co.jp>
2018-01-01 23:01:57 +0900

    パラメータ追加#

--- src/main.py
+++ src/main.py
@@ -7,3 +7,4 @@ def main():
     parser.add_argument('--env_name', default='PongNoFrameskip-v4', help='Environment name')
     parser.add_argument('--width', type=int, default=84, help='Width of resized frame')
     parser.add_argument('--height', type=int, default=84, help='Height of resized frame')
+    parser.add_argument('--workers', type=int, default=16, help='How many training processes to use')
